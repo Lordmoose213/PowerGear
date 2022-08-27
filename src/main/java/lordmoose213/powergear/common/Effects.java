4d782a4402be9580021975a06aeefbcd8e295ae8
Lordmoose213 <lucaseaton05@icloud.com>
2022-08-27 17:43:35 -0400

    Oops forgot to commit this with the prev commit

---
+++ src/main/java/lordmoose213/powergear/common/Effects.java
@@ -0,0 +1,8 @@
+package lordmoose213.powergear.common;
+
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+
+public class Effects {
+    public static MobEffectInstance helmetNightVision = new MobEffectInstance(MobEffects.NIGHT_VISION, 400, 0, true, true, true);
+}
